0e9820c5e6674b3c2c301ce08bf706436ec1d7d8
rajatasusual <krajat4@gmail.com>
2021-08-14 12:07:20 +0530

    Fixing the mail headers

--- mail.php
+++ mail.php
@@ -7,9 +7,9 @@ $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
 $text = trim($_POST["text"]);
 $message = "Name: $name \nEmail: $email \nText: $text";
-$headers = "From: Contact Form <contact@rajatasusual.online> \r\n";
-$headers .= "Content-type: text/plain; charset=\"utf-8\"\r\n ";
-$headers .= "Reply-To:" . $email . "\r\n" ."X-Mailer: PHP/" . phpversion();
+$headers = "From: Contact Form <contact@rajatasusual.online>"."\r\n";
+$headers .= "Reply-To:" . $email . "\r\n";
+$headers .= "X-Mailer: PHP/" . phpversion();
 
-$pagetitle = "New Contact Form Submitted \"$sitename\"";
-mail($recepient, $pagetitle, $message, $headers);
+$subject = "New Contact Form Submitted \"$sitename\"";
+mail($recepient, $subject, $message, $headers);
