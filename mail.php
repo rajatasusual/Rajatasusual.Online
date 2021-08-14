6485748faed55140f9c70ff3a24edb90cdcaf50a
rajatasusual <krajat4@gmail.com>
2021-08-14 11:55:58 +0530

    Fixing Reply-To in mail

--- mail.php
+++ mail.php
@@ -8,7 +8,7 @@ $email = trim($_POST["email"]);
 $text = trim($_POST["text"]);
 $message = "Name: $name \nEmail: $email \nText: $text";
 $headers = "From: Contact Form <contact@rajatasusual.online> \r\n";
-$headers .= "Content-type: text/plain; charset=\"utf-8\"\n ";
+$headers .= "Content-type: text/plain; charset=\"utf-8\"\r\n ";
 $headers .= "Reply-To:" . $email . "\r\n" ."X-Mailer: PHP/" . phpversion();
 
 $pagetitle = "New Contact Form Submitted \"$sitename\"";
