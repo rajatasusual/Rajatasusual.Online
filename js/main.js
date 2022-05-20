8cf80758c4f2ab4be280f0ad37aad323d9952bd6
Rajat Kumar <krajat4@gmail.com>
2022-05-20 19:26:39 +0200

    mail.php was not relatively referenced in JS

--- js/main.js
+++ js/main.js
@@ -472,7 +472,7 @@ $(function () {
     $("#form").submit(function () {
       $.ajax({
         type: "POST",
-        url: "mail.php",
+        url: "/mail.php",
         data: $(this).serialize()
       }).done(function () {
 
